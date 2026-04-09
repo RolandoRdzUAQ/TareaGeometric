9bd6ebcd2f3cfb354515cdd28ab139b169ed515b
Rolando Rodriguez <rodriguez.castro.rolando@gmail.com>
2026-04-09 01:23:57 -0600

    feat: Add Class_Rectangle extending GeometryObject

---
+++ src/Principal/Class_Rectangle.java
@@ -0,0 +1,26 @@
+package Principal;
+
+public class Class_Rectangle extends GeometryObject {
+    private double width;
+    private double height;
+
+    public Class_Rectangle() {}
+
+    public Class_Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Class_Rectangle(double width, double height, String color, boolean filled) {
+        super(color, filled);
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() { return width; }
+    public double getHeight() { return height; }
+    public void setWidth(double width) { this.width = width; }
+    public void setHeight(double height) { this.height = height; }
+    public double getArea() { return width * height; }
+    public double getPerimeter() { return 2 * (width + height); }
+}
